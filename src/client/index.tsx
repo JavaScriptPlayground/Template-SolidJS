4be79ab7ce443bcde06bc28ec9a9ac418ac8b1d0
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-07-17 15:19:32 +0200

    Initial commit

---
+++ src/client/index.tsx
@@ -0,0 +1,13 @@
+import { render } from '@solid-js/web';
+import { HashRouter, Route } from '@solid-js/router'
+import { Home } from './pages/home/index.tsx';
+import { NotFound } from './pages/404/index.tsx';
+
+
+render(
+  () => <HashRouter>
+    <Route path='/' component={Home}/>
+    <Route path='/404' component={NotFound}/>
+  </HashRouter>,
+  globalThis.document.body
+);
---
+++ src/client/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import type {JSXElement} from '@solid-js'
+
+export function Home() : JSXElement {
+  return (
+    <div class='page home'>
+      <h1 class='title'>Home</h1>
+      <img class='logo' src='/static/solidjs_logo.svg' alt='SolidJS Logo'/>
+      <a class='link' href='/404'>Go to 404</a>
+    </div>
+  )
+}
