b26aff8605a71ebe0854a28780175adbd97f0b81
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-07-17 16:27:28 +0200

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,7 @@
 // --server-root="./src/client"
+Deno.serve({
+    port: 80
+}, (request) => {
+    console.log(new URL(request.url).pathname);
+    return new Response(Deno.readFileSync('./src/client/index.html'));
+})
