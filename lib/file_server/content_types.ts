0709a2eda3d066a7d6a45bcaf389ae1301119e48
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-07-17 16:44:19 +0200

    Update content_types.ts

--- lib/file_server/content_types.ts
+++ lib/file_server/content_types.ts
@@ -6,7 +6,12 @@ export const contentTypes = {
     jpeg: 'image/jpeg',
     jpg: 'image/jpeg',
     js: 'text/javascript',
-    json: 'application/json'
+    json: 'application/json',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    woff: 'font/woff',
+    woff2: 'font/woff2',
+    xhtml: 'application/xhtml+xml'
 }
 
 export type contentType = typeof contentTypes;
