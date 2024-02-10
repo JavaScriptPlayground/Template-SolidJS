024de4703faa9c758b67025fe29ba4c227275f50
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-11 00:40:10 +0100

    Update build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -35,6 +35,8 @@ const filesConfig : esbuild.BuildOptions = {
   color: true,
   minify: !args.develope ?? true,
   outdir: './dist',
+  sourcemap: true,
+  sourcesContent: true,
   entryNames: '[dir]/bundle.min',
   entryPoints: [
     './src/**/index.ts',
