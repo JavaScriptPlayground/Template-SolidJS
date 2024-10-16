664d801dcae06df4820ec7b249c743f4df2e50b3
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-10-16 08:37:18 +0200

    Removed

--- src/client/_app/index.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from "@solid-js/web";
-import { Counter } from '../_components/counter.tsx';
-
-// deno-lint-ignore no-non-null-assertion no-undef
-render(() => <Counter />, document.getElementById("app")!);
