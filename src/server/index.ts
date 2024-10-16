2f447e3603c8baf4b864d7f9e5175f5de086d91a
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-10-16 07:52:55 +0200

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,34 @@
-import { serveDir } from "@std/http/file-server";
-import { parseArgs } from "@std/cli/parse-args";
+import { route, type Route } from "@std/http/unstable-route";
+import { serveDir, serveFile } from "@std/http/file-server";
 
-const args = parseArgs<{
-  develope: boolean | undefined,
-  port: number
-}>(Deno.args);
+const routes: Route[] = [
+  // {
+  //   pattern: new URLPattern({ pathname: "/static/*" }),
+  //   handler: (request: Request) => serveDir(request)
+  // },
+  // {
+  //   pattern: new URLPattern({ pathname: "/component" }),
+  //   handler: (request: Request) => serveDir(
+  //     request,
+  //     {fsRoot: './dist/component/'}
+  //   )
+  // },
+  {
+    pattern: new URLPattern({ pathname: "/:page" }),
+    handler: (request, _info, parameters) => {
+      
+      console.log('/dist/route/' + (parameters?.pathname.groups.page || 'home'));
+      
+      return serveFile(
+        request,
+        '/dist/route/' + (parameters?.pathname.groups.page ?? 'home')
+      )
+    }
+  }
+];
 
-Deno.serve({port: args.port ?? 80}, (req) => {
-  return serveDir(req, {
-    fsRoot: './dist/',
-    showDotfiles: true,
-    quiet: !args.develope,
-    showDirListing: args.develope
-  });
-});
+function defaultHandler(_req: Request) : Response {
+  return new Response("Not found", { status: 404 });
+}
+
+Deno.serve(route(routes, defaultHandler));
