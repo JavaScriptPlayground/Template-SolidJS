e6a970f97a2c4d1cc28e4ddd38c060af8e32e861
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-10-17 10:50:06 +0200

    Update page_handler.ts

--- src/server/request_handler/page_handler.ts
+++ src/server/request_handler/page_handler.ts
@@ -4,17 +4,19 @@ import { serveFile } from '@std/http/file-server';
  * This function handles page requests.
  * 
  * @param request The request from the client
+ * @param root The root directory
  * @param page The requested page
- * @param defaultFile The default file to serve
+ * @param file The file to serve
  * @returns The page response
  */
 export default function pageHandler(
   request : Request,
+  root : string,
   page : string | undefined,
-  defaultFile = 'index.html'
+  file : string = 'index.html'
 ) : Promise<Response> {
   return serveFile(
     request,
-    `./dist/route/${page}/${defaultFile}`
+    `${root}/${page}/${file}`
   )
 }
