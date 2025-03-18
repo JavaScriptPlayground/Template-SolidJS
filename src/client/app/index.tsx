8de8045f2a17174bafe55131ae79d36b721fe794
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-03-18 10:46:03 +0100

    Update index.tsx

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -1,22 +1,6 @@
 import { render } from "@solid-js/web";
 import { Route, Router } from "@solid-js/router";
-import { createSignal, type JSXElement } from "@solid-js";
 import { Home } from './pages/home/index.tsx';
-
-function App() : JSXElement {
-  const [count, setCount] = createSignal(1);
-  const increment = () => setCount(count => count + 1);
-
-  return (
-    <div class='app'>
-      <h1 class='title'>Home</h1>
-      <img class='logo' src='/static/solidjs_logo.svg' alt='SolidJS Logo' />
-      <button type='button' onClick={increment}>
-        {count()}
-      </button>
-    </div>
-  );
-}
 
 const root = globalThis.document.getElementById("root")
 if (!root) {
